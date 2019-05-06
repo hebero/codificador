3676b70ab0a17e4c26f39049c49f9f3ef8a38b61
Heber <heberdanielorellana@gmail.com>
2019-05-05 18:06:06 -0600

    Funcion de decodificar esta terminada.

--- funciones.scm
+++ funciones.scm
@@ -1,8 +1,8 @@
 ; PUEDE AGREGAR MÁS FUNCIONES E INSTRUCCIONES SEGÚN SU NECESIDAD
 
 
-; encodeChar codifica un caracter en base a otro caracter de llave
-(define (encodeChar chTxt chLlave)
+; decodeChar decode un caracter en base a otro caracter de llave
+(define (decodeChar chTxt chLlave)
     ;su implementación
     #\null
 )
@@ -10,14 +10,20 @@
 ; encodeString codifica un string utilizando la llave parametrizada
 (define (encodeString txt llave)
    ;su implementación
+   
+   
    ""
 )
+(define (getNextChar llave)
+	
+)
 
-; decodeChar decode un caracter en base a otro caracter de llave
-(define (decodeChar chTxt chLlave)
+; encodeChar codifica un caracter en base a otro caracter de llave
+(define (encodeChar chTxt chLlave)
    ;su implementación
    (define chTxtIsLowerCase false)
    (define chLlaveIsLowerCase false)
+   
    (cond
         [(and (< (char->integer chTxt) 91) (> (char->integer chTxt) 64))
             (set! chTxtIsLowerCase false)
@@ -36,21 +42,28 @@
         ]
    )
    
-  (lambda (x y)
-  (
-    (+ (+ (- x 64) (- 64 y)) 1)
-  )
-    
-  )
-
-  
-   #\null
+  (convertChar (lambda (x y)((+ (+ x y) 1))
+    (convertChar chTxt chTxtIsLowerCase 1) (convertChar chLlave chLlaveIsLowerCase 1)
+  ) chTxtIsLowerCase 2)
 )
-(define (getNeutralNumber chr isLowercase)
+(define (convertChar chr isLowercase mode)
     (cond
-    [(equal? isLowercase true)
-
-    ]
+	[(= mode 1)
+		[(equal? isLowercase false)
+			(- chr 64)
+		]
+		[else
+			(- chr 96)
+		]
+	]
+	[(= mode 2)
+		[(equal? isLowercase false)
+			(+ chr 64)
+		]
+		[else
+			(+ chr 96)
+		]
+	]
     )
 )
 ; decodeString decodifica un strgin utilizando la llave parametrizada
